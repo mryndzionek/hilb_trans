fc9de9ed6f956521db95c154ed5f3c0d1338513e
Mariusz Ryndzionek <mryndzionek@gmail.com>
2025-10-16 21:17:31 +0200

    Small refactor and generalization

--- des_hilb_allpass.py
+++ des_hilb_allpass.py
@@ -10,75 +10,66 @@ def to_hilb(a):
     return a * h
 
 
+def tfmul(a, b):
+    return np.flip(np.polymul(np.flip(a), np.flip(b)))
+
+
 # taps of a two-branch, two-section, lowpass half-band filter
 alphas = [6.27978583e-01, 3.56629192e-01, 1.07224663e-01, 8.74907424e-01]
 alphas = np.array(alphas)
 
-b00 = [alphas[0], 0, 1]
-a00 = [1, 0, alphas[0]]
+b0 = []
+a0 = []
+for i in range(0, alphas.size, 2):
+    b0.append([alphas[i], 0, 1])
+    a0.append([1, 0, alphas[i]])
 
-b01 = [alphas[2], 0, 1]
-a01 = [1, 0, alphas[2]]
+b1 = []
+a1 = []
+for i in range(1, alphas.size, 2):
+    b1.append([alphas[i], 0, 1])
+    a1.append([1, 0, alphas[i]])
 
-b10 = [alphas[1], 0, 1]
-a10 = [1, 0, alphas[1]]
 
-b11 = [alphas[3], 0, 1]
-a11 = [1, 0, alphas[3]]
+b0 = list(map(to_hilb, b0))
+a0 = list(map(to_hilb, a0))
 
-w, h00 = sig.freqz(b00, a00)
-w, h01 = sig.freqz(b01, a01)
+b1 = list(map(to_hilb, b1))
+a1 = list(map(to_hilb, a1))
 
-w, h10 = sig.freqz(b10, a10)
-w, h11 = sig.freqz(b11, a11)
+h0 = 1
+bi = 1
+ai = 1
+for b, a in zip(b0, a0):
+    w, h = sig.freqz(b, a, whole=True)
+    h0 *= h
+    bi = tfmul(bi, b)
+    ai = tfmul(ai, a)
 
-w, hd = sig.freqz([0, 1], 1)
-h = h00 * h01 + h10 * h11 * hd
+_, h1 = sig.freqz([0, 1], 1, whole=True)
+bq = [0, 1]
+aq = 1
+for b, a in zip(b1, a1):
+    w, h = sig.freqz(b, a, whole=True)
+    h1 *= h
+    bq = tfmul(bq, b)
+    aq = tfmul(aq, a)
 
-# plt.plot(w, np.unwrap(np.angle(h)))
+# h = h0 + 1j * h1
+# plt.plot(w, np.unwrap(np.angle(h0)))
+# plt.plot(w, np.unwrap(np.angle(h1)))
+# plt.grid(True)
 # plt.show()
+
 # plt.plot(w, 20 * np.log10(np.abs((h) / 2)))
+# plt.ylim(-80, 1)
+# plt.ylabel("Magnitude (dB)")
+# plt.xlabel("radian/sample")
+# plt.grid(True)
 # plt.show()
 
-b00 = to_hilb(b00)
-a00 = to_hilb(a00)
-b01 = to_hilb(b01)
-a01 = to_hilb(a01)
-b10 = to_hilb(b10)
-a10 = to_hilb(a10)
-b11 = to_hilb(b11)
-a11 = to_hilb(a11)
-
-w, h00 = sig.freqz(b00, a00, whole=True)
-w, h01 = sig.freqz(b01, a01, whole=True)
-
-w, h10 = sig.freqz(b10, a10, whole=True)
-w, h11 = sig.freqz(b11, a11, whole=True)
-
-w, hd = sig.freqz([0, 1], 1, whole=True)
-
-h = h00 * h01 + 1j * h10 * h11 * hd
-
-# plt.plot(w, np.unwrap(np.angle(h)))
-# plt.show()
-# plt.plot(w, 20 * np.log10(np.abs((h) / 2)))
-# plt.ylim(-60, 1)
-# plt.show()
-
-bi = np.polymul(np.flip(b00), np.flip(b01))
-ai = np.polymul(np.flip(a00), np.flip(a01))
-ai = np.real(np.flip(ai))
-bi = np.real(np.flip(bi))
-
-bq = np.polymul(np.flip(b10), np.flip(b11))
-bq = np.polymul(bq, [0] * (bq.size - 2) + [1, 0])
-aq = np.polymul(np.flip(a10), np.flip(a11))
-
-aq = np.real(np.flip(aq))
-bq = np.real(np.flip(bq))
-
-print(list(map(float, bi)), list(map(float, ai)))
-print(list(map(float, bq)), list(map(float, aq)))
+print(list(map(float, np.real(bi))))
+print(list(map(float, np.real(bq))))
 
 w, hi = sig.freqz(bi, ai, whole=True)
 w, hq = sig.freqz(bq, aq, whole=True)
